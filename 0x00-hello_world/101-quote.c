cb2ac48203b6182d5f51ba610140343ec0a9311a
Noah Binche <binchenoah@gmail.com>
2022-06-03 09:56:52 -0700

    Print a quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - Entry to the program
+  *
+  * Print a quote from Dora Korpar to stderr
+  *
+  * return: 1 (success)
+  */
+int main()
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return 1;
+}

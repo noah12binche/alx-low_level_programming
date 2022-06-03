b9cef939961c2ef90614274ecd7054c2227abd6d
Noah Binche <binchenoah@gmail.com>
2022-06-03 04:42:09 -0700

    Print strings to stdout

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *Description: main - Prints out a sring to stdout
- *Return: Always 0 (success)
- */
+  * Description: main - Prints out a string to stdout.
+  * Return: 0 (success)
+  */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");

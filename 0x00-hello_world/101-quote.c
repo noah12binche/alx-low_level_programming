d9a1790e030c0c7d1bc0985c2e44d0575a108309
Noah Binche <binchenoah@gmail.com>
2022-06-03 10:06:48 -0700

    Add comma to lines

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
   *
   * return: 1 (success)
   */
-int main()
+int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return 1;
+    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+    return (1);
 }

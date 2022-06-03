0d88ef5b56fc8458a0fcefb16f987caa380661ff
Noah Binche <binchenoah@gmail.com>
2022-06-03 01:40:34 -0700

    Include commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- Description: main - Prints out a sring to stdout
- Return: 0 if success
- */
+ *Description: main - Prints out a sring to stdout
+ *Return: 0 if success
+*/
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");

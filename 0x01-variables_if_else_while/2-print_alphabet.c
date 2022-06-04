25fd537217675611754105413f208c16237f09a1
Noah Binche <binchenoah@gmail.com>
2022-06-03 21:03:00 -0700

    Prints alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the alphabets
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'x'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+
+}

753be753bb963147f9c04304b050453dd7ad2717
Noah Binche <binchenoah@gmail.com>
2022-06-05 00:38:14 -0700

    Print base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - Prints numbers from 0 t0 9
+  *
+  *Return: Always (Success)
+  */
+int main(void)
+
+{
+	char n;
+
+	for (n = '0'; n <= '9'; c++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

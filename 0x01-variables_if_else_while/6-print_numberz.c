746aa29df3e65a58e39cf6d4fbfd3da62d86e11d
Noah Binche <binchenoah@gmail.com>
2022-06-05 00:49:25 -0700

    Print numbers 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Prints numbers from 0 to 9
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

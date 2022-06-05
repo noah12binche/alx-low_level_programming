9b9ee2206813ac0a9c8e376b5980dd3cfb3ad916
Noah Binche <binchenoah@gmail.com>
2022-06-05 05:04:47 -0700

    Combination of three number

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,18 +15,20 @@ int main(void)
 		{
 			for (k = '0'; k <= '9'; k++)
 			{
-				putchar(c);
-				putchar(i);
-				putchar(k);
-
-				if (c != '7')
+				if (c < i && i < k)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(c);
+					putchar(i);
+					putchar(k);
+
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
-
 	}
 
 	putchar('\n');

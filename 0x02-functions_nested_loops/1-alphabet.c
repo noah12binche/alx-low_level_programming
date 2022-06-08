03e45127d1cd2032961116f1209e3b5c30648eb8
Noah Binche <binchenoah@gmail.com>
2022-06-08 12:12:18 -0700

    Print alphabet lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * main - Print alphabet
+  *
+  * Return: 0
+  */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+
+	_putchar('\n');
+}

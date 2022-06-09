e0944a46ada03347ba593e08aed86c24c9faaddb
Noah Binche <binchenoah@gmail.com>
2022-06-09 09:24:14 -0700

    Print different triangels

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,3 +1,10 @@
+#include "main.h"
+
+/**
+ * print_triangle -  a function that prints a triangle, followed by a new line.
+ * @size: An input integer
+ * Return: Always 0
+ */
 void print_triangle(int size)
 {
 	int i = 0, j, n = size - 1;

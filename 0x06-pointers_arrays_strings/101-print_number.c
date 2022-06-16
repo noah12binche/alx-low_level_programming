d7bedf108c9db74d1af3a21a9ffe3688d3adb8cb
Noah Binche <binchenoah@gmail.com>
2022-06-16 15:50:38 -0700

    Print when it is a number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,35 @@
 #include "main.h"
 
 /**
-   * print_number - print numbers chars
-   * @n: integer params
-   * Return: 0
-  **/
-
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
 void print_number(int n)
 {
-unsigned int n1;
+	unsigned int m, d, count;
 
-n1 = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
 
-if (n < 0)
-{
-putchar('-');
-n1 = -n;
-}
+	d = m;
+	count = 1;
 
-if (n1 / 10 != 0)
-{
-print_number(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
